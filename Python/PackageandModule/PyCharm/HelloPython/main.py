ec5157b30c6e05edc82ed683e98b14ae62e79ce8
Adem Yıldız <ademy0783@gmail.com>
2025-07-28 00:42:08 +0300

    PackageandModule

---
+++ Python/PackageandModule/PyCharm/HelloPython/main.py
@@ -0,0 +1,18 @@
+print("hello python")
+
+from random import randint
+# import ademmodule
+from ademmodule import example_func
+from AnimalPackage import info
+from AnimalPackage.CatPackage import meow # sub package
+import numpy
+
+print(numpy.zeros((3,4)))
+
+# ademmodule.example_func()
+example_func()
+
+info.info()
+# meow.speak()
+
+meow.test()
