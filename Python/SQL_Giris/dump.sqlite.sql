0f0c0fb480ddaadfa1400f7d5f24204f809ec018
Adem Yıldız <ademy0783@gmail.com>
2025-07-30 13:23:43 +0300

    SQL_Giris

---
+++ Python/SQL_Giris/dump.sqlite.sql
@@ -0,0 +1,10 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE Students (id INTEGER PRIMARY KEY, name VARCHAR, surname VARCHAR);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
